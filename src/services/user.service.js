996952d17d2db130a9f8d32a54e0b42743fff982
melting <meltcahaeng@gmail.com>
2021-10-04 23:23:35 +0800

    03用户注册接口信息保存

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,17 @@
-class UserService{
-    async create(user){
+const connection = require('../app/database')
+
+class UserService {
+    async create(user) {
+
+        const {name,password} = user;
+
+        const statement = `INSERT INTO user (name,password) VALUES (?, ?);`
+
+        const result = await connection.execute(statement,[name,password])
         //将user存储到数据库中
-        console.log("将用户数据保存到数据库",user);
-        return "创建用户成功"
+        // console.log("将用户数据保存到数据库",user);
+        return result
+        // return "创建用户成功"
     }
 }
 
