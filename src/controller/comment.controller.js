e868317187f769a2a85c210dfd09118a5db13327
melting <meltcahaeng@gmail.com>
2021-10-07 15:54:07 +0800

    18给评论发表评论

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -7,6 +7,13 @@ class CommentController{
         const result = await service.create(momentId,content,id)
         ctx.body = result
     }
+    async reply(ctx,next){
+        const {commentId} = ctx.params
+        const {momentId,content} = ctx.request.body
+        const {id} = ctx.user
+        const result = await service.reply(momentId,content,id,commentId)
+        ctx.body = result
+    }
 }
 
 module.exports = new CommentController()
--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -6,6 +6,11 @@ class CommentService{
         const [result] = await connection.execute(statement,[content,momentId,userId])
         return result
     }
+    async reply(momentId,content,userId,commentId){
+        const statement = `INSERT INTO comment(content,moment_id,user_id,comment_id) VALUE(?,?,?,?);`
+        const [result] = await connection.execute(statement,[content,momentId,userId,commentId])
+        return result
+    }
 }
 
 module.exports = new CommentService()
